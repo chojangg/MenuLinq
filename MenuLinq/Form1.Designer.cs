60178d40eedaceb83282ab38cee5511e6ba7a59f
chojangg <s2115@e-mirim.hs.kr>
2023-08-10 14:41:48 +0900

    버튼, 배경색상 추가

--- MenuLinq/Form1.Designer.cs
+++ MenuLinq/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.starDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.menuBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -49,6 +49,7 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nameDataGridViewTextBoxColumn,
@@ -62,56 +63,6 @@
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(770, 336);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(18, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 74);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "전체 메뉴 보기";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(174, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 74);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "이름순";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(329, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(147, 74);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "가격 낮은순";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(484, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(147, 74);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "음료";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(641, 12);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(147, 74);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "평점 4점이상";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // nameDataGridViewTextBoxColumn
             // 
@@ -144,6 +95,61 @@
             // menuBindingSource
             // 
             this.menuBindingSource.DataSource = typeof(MenuLinq.Menu);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Location = new System.Drawing.Point(18, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "전체 메뉴 보기";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Location = new System.Drawing.Point(174, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 74);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "이름순";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button3.Location = new System.Drawing.Point(329, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 74);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "가격 낮은순";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button4.Location = new System.Drawing.Point(484, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 74);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "음료";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button5.Location = new System.Drawing.Point(641, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 74);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "평점 4점이상";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
